f6265eaab2498cb63e3d6ddac5c94a6345c3136c
guptaadi-ux <52713840+guptaadi-ux@users.noreply.github.com>
2019-07-24 00:26:58 -0400

    Update mySongs.ts

--- assets/data/mySongs.ts
+++ assets/data/mySongs.ts
@@ -8,28 +8,28 @@ export const songs: songInfo[] = [
     artist: 'Karan Aujla',
     genre: 'Pop',
     yearReleased: 2018,
-    picture: "../assets/image/10.jpg"
+    picture: "../../assets/image/10.jpg"
 },
 {
 songName: 'Sohne Lagde',
 artist: 'Sidhu Moose Wala',
 genre: 'Pop',
 yearReleased: 2018,
-picture: "../assets/image/11.jpg"
+picture: "../../assets/image/11.jpg"
 },
 {
 songName: 'Munda Manaka Da',
 artist: 'Jass Manak',
 genre: 'Pop',
 yearReleased: 2018,
-picture: "../assets/image/12.jpg"
+picture: "../../assets/image/12.jpg"
 },
 {
 songName: 'Diwana',
 artist: 'Bohemia',
 genre: 'Hip Hop',
 yearReleased: 2009,
-picture: "../assets/image/13.jpg"
+picture: "../../assets/image/13.jpg"
 }
 
 ]
