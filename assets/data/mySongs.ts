d027a3b4b23df4fb7f6195b2cabd647fc80a0678
guptaadi-ux <52713840+guptaadi-ux@users.noreply.github.com>
2019-07-23 23:06:14 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,36 @@
+import { songInfo } from '../../app/songInfo';
+
+
+
+export const songs: songInfo[] = [
+{
+    songName: 'Facts',
+    artist: 'Karan Aujla',
+    genre: 'Pop',
+    yearReleased: 2018,
+    picture: "../assets/image/10.jpg"
+},
+{
+songName: 'Sohne Lagde',
+artist: 'Sidhu Moose Wala',
+genre: 'Pop',
+yearReleased: 2018,
+picture: "../assets/image/11.jpg"
+},
+{
+songName: 'Munda Manaka Da',
+artist: 'Jass Manak',
+genre: 'Pop',
+yearReleased: 2018,
+picture: "../assets/image/12.jpg"
+},
+{
+songName: 'Diwana',
+artist: 'Bohemia',
+genre: 'Hip Hop',
+yearReleased: 2009,
+picture: "../assets/image/13.jpg"
+}
+
+]
+
